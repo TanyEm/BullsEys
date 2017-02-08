8d707667d130b7546ce2d9077f7cd711d32e17a2
Tanya Tomchuk <tanyem@protonmail.com>
2017-02-08 15:45:19 +0300

    First comment. New project. Created interface. showAlert and sliderMoved is works

--- BullsEys/ViewController.swift
+++ BullsEys/ViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var currentValue = 0
+    @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        currentValue = lroundf(slider.value)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +24,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showAlert() {
+        let message = "The value of the slider is now: \(currentValue)"
+        let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    @IBAction func sliderMoved(_ slider : UISlider) {
+        currentValue = lroundf(slider.value)
+    }
 }
+
 
